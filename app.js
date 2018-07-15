1f717d9a5588d70f5b0f34c5bf027892051021c4
nicosp <nicolassanpedro@gmail.com>
2018-07-15 20:32:56 -0300

    address & geo reqs

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=2360%20cordoba%20street%20buenos%20aires',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Coordenades: Latitude => ${body.results[0].geometry.location.lat} && Longitude => ${body.results[0].geometry.location.lng}`);
+
 });
