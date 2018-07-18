c97a748755d8ef4db1119fc288b0b594737e6dcd
nicosp <nicolassanpedro@gmail.com>
2018-07-17 22:16:02 -0300

    mods

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+    .options({
+        a: {
+          demand: true,
+          alias: 'address',
+          describe: 'Address to fetch weather',
+          string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+  console.log(argv);
+  console.log(argv.address);
+
+let encodeAddress = encodeURIComponent(argv.address);
+console.log(encodeAddress);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=2360%20cordoba%20street%20buenos%20aires',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
