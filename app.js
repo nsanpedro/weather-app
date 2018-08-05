5e763238e7ba943295dff2c04a82199617875e61
nicosp <nicolassanpedro@gmail.com>
2018-08-05 20:47:24 -0300

    weatherApi Integration

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const argv = yargs
-    .options({
-        a: {
-          demand: true,
-          alias: 'address',
-          describe: 'Address to fetch weather',
-          string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//     .options({
+//         a: {
+//           demand: true,
+//           alias: 'address',
+//           describe: 'Address to fetch weather',
+//           string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+//     if(errorMsg) {
+//       console.log(errorMsg);
+//     } else {
+//       console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+//
+// // f54ab10f09094cb79d58fc393bdfeb91
+//
+//
 
-geocode.geocodeAddress(argv.address, (errorMsg, results) => {
-    if(errorMsg) {
-      console.log(errorMsg);
-    } else {
-      console.log(JSON.stringify(results, undefined, 2));
-    }
+
+const request = require('request');
+
+request({
+  url: 'https://api.darksky.net/forecast/f54ab10f09094cb79d58fc393bdfeb91/-34.6036844,-58.3815591',
+  json: true
+}, (err, response, body) => {
+  if(!err && response.statusCode === 200) {
+    console.log(`The temperature in the requested city is ${body.currently.temperature} !`);
+  } else {
+    console.log('Unable to Fetch data from API');
+  }
+
 });
